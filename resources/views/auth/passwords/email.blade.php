818ee714ee7a6b5a53139f001245e5d62daa048d
Shea Lewis <shea.lewis89@gmail.com>
2016-09-06 09:00:30 -0700

    Minor UI tweaks

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default panel-login">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading"><h3 class="panel-title">Reset Password</h3></div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default panel-login">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading"><h3 class="panel-title">Reset Password</h3></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
