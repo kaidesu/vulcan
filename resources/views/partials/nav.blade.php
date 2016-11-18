2d1f7a364cf4b4d207fa8237f0cb46c6d1c5ad5f
Shea Lewis <shea.lewis89@gmail.com>
2016-11-18 19:34:07 +0000

    UI tweaks

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,30 +1,33 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+    <div class="container-fluid container-nav">
 
-            <a class="navbar-brand" href="#">Vulcan</a>
-        </div>
+            <div class="navbar-header col-sm-3 col-md-2">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="{{ url('/logout') }}"
+                <a class="navbar-brand" href="#">Vulcan</a>
+            </div>
+
+        <div class="col-sm-9 col-md-10">
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="{{ url('/logout') }}"
                         onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
+                        document.getElementById('logout-form').submit();">
                         Logout
                     </a>
 
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            </ul>
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
