7c9bffad0d9511618fac3d75912c5a11b655acbe
Shea Lewis <shea.lewis89@gmail.com>
2017-11-26 11:10:00 -0800

    Theme updates

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,5 +1,5 @@
 <div class="page-content__header">
-    <div class="navbar navbar-dark bg-dark">
+    <div class="navbar navbar-dark bg-dark bg-gradient">
         <span class="navbar-text">
             <span class="fas fa-bars fa-fw"></span>
         </span>
--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="sidebar column">
     <div class="sidebar__header">
         <a href="/">
-            <span class="fas fa-hand-spock fa-fw mr-2" aria-hidden="true"></span> Vulcan
+            Vulcan
         </a>
     </div>
     
@@ -12,13 +12,6 @@
                     <a href="/" class="sidebar__link">
                         <span class="fas fa-home fa-fw sidebar__icon"></span>
                         Home
-                    </a>
-                </li>
-                
-                <li class="sidebar__item">
-                    <a href="/preface" class="sidebar__link">
-                        <span class="fas fa-leaf fa-fw sidebar__icon"></span>
-                        Preface
                     </a>
                 </li>
                 
